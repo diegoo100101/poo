bf584ba7c5ef3d0191017fe0a5999e79b53f81bb
Diego Cantoral <diego.cantoral@axity.com>
2025-05-19 11:33:07 -0600

    add herencia.ts and index.html

---
+++ herencia.ts
@@ -0,0 +1,16 @@
+class Vehiculo {
+    public marca: string;
+    public modelo: string;
+
+    constructor(marca: string, modelo: string) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public arrancar(): void {
+        
+    }
+
+    public detener(): void {
+    }
+}
